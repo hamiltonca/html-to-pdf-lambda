9f4489eecc485499f4168d45867a8e41e4e7b068
Chris Hamilton <chamilton-dev@compucafe.com>
2021-01-15 13:37:49 -0500

    Add try/catch/finally to close browser in all cases.

--- index.js
+++ index.js
@@ -5,30 +5,42 @@ exports.handler = async (event, context) => {
     console.log('## CONTEXT: ' + JSON.stringify(context))
     console.log('## EVENT: ' + JSON.stringify(event))
 
-    const reqContent = event.body
-    console.log("request content: " + reqContent)
-    console.log("launching browser... IS_OFFLINE: " + process.env.IS_OFFLINE)
-    const browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true
-    });
-    console.log("browser launched, setting up page.");
-    let page = await browser.newPage();
-    await page.setContent(reqContent)
-    console.log("page content set... generating pdf")
-    const pdf = await page.pdf({
-        format: "letter",
-        printBackground:true
-    })
-    console.log("closing browser")
-    await browser.close()
-    console.log("browser closed, returning content")
-    const retContent = formatResponse(pdf.toString('utf-8'))
-    console.log("retContent: " + retContent)
-    return formatResponse(retContent)
+    let browser;
+    try {
+        const reqContent = event.body
+        console.log("request content: " + reqContent)
+        console.log("launching browser... IS_OFFLINE: " + process.env.IS_OFFLINE)
+        browser = await chromium.puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true
+        });
+        console.log("browser launched, setting up page.");
+        let page = await browser.newPage();
+        await page.setContent(reqContent)
+        console.log("page content set... generating pdf")
+        const pdf = await page.pdf({
+            format: "letter",
+            headless: true,
+            printBackground:true
+        })
+        const retContent = formatResponse(pdf.toString('utf-8'))
+        console.log("retContent: " + retContent)
+        return formatResponse(retContent)
+
+    }
+    catch(e) {
+        console.error("exception caught " + e)
+    }
+    finally {
+        if (browser) {
+            console.log("closing browser")
+            browser.close()
+            console.log("browser closed, returning content")
+        }
+    }
 }
 const formatResponse = function(body) {
     console.log("formatting response")
